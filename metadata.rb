7d94e808b68733010bfca5a06578847cda4309d5
Eric Saxby & Konstantin Gredeskoul <pair+sax+kig@wanelo.com>
2012-12-03 13:38:51 -0800

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Wanelo, Inc"
+maintainer_email "ops@wanelo.com"
+license          "Apache 2.0"
+description      "Installs/Configures postgres from sources"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports "smartos"
