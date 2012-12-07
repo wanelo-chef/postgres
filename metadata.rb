b139b636dd5d1ab6d12490e1d1c063465cb4aa72
Eric Saxby & Konstantin Gredeskoul <pair+sax+kig@wanelo.com>
2012-12-07 12:31:41 -0800

    Stop PG fast when shutting down

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email "ops@wanelo.com"
 license          "Apache 2.0"
 description      "Installs/Configures postgres from sources, optimized for SmartOS, and ready for streaming replication"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 supports "smartos"
+
+depends  "rbac"
+depends  "smf"
