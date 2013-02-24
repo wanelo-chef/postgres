4b244518856f76824f414d9464d97eb160b32957
Eric Saxby <sax@livinginthepast.org>
2013-02-24 12:45:05 -0800

    Format change

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             "postgres"
-maintainer       "Wanelo, Inc"
-maintainer_email "ops@wanelo.com"
-license          "Apache 2.0"
-description      "Installs/Configures postgres from sources, optimized for SmartOS, and ready for streaming replication"
+name             'postgres'
+maintainer       'Wanelo, Inc'
+maintainer_email 'ops@wanelo.com'
+license          'Apache 2.0'
+description      'Installs/Configures postgres from sources, optimized for SmartOS, and ready for streaming replication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.9"
+version          '0.0.9'
 
-supports "smartos"
+supports 'smartos'
 
-depends  "rbac"
-depends  "smf"
+depends 'rbac'
+depends 'smf'
