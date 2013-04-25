7512b95314e71146ab919ef719d80262ca955cd7
Eric Saxby <sax@livinginthepast.org>
2013-04-25 00:05:31 -0700

    Update copyright year

--- recipes/contrib.rb
+++ recipes/contrib.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: postgres
 # Recipe:: contrib
 #
-# Copyright 2012, Wanelo, Inc.
+# Copyright 2013, Wanelo, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
