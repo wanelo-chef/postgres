f0722cde971e5e8b6cb7263a5cc0fe4337227ff4
Eric Saxby <sax@livinginthepast.org>
2013-09-26 17:42:46 -0700

    Depend on more recent smf

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'ops@wanelo.com'
 license          'Apache 2.0'
 description      'Installs/Configures postgres from sources, optimized for SmartOS, and ready for streaming replication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.1.0'
 
 supports 'smartos'
 
 depends 'resource-control'
 depends 'rbac'
-depends 'smf', '>= 0.6.8'
+depends 'smf', '>= 2.0.1'
 depends 'ipaddr_extensions'
