b4f761abf064ab66b06b77d2681a52d7a8c12662
Eric Saxby & James Hart <pair+sax+james@wanelo.com>
2014-02-18 14:12:05 -0800

    Link libpq.so.5 into /opt/local/lib

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@wanelo.com'
 license          'Apache 2.0'
 description      'Installs/Configures postgres from sources, optimized for SmartOS, and ready for streaming replication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.6.0'
+version          '2.7.0'
 
 supports 'smartos'
 
--- recipes/symlinks.rb
+++ recipes/symlinks.rb
@@ -15,3 +15,15 @@ version_specific_bin_dir = prefix_dir + "/bin"
     to "#{version_specific_bin_dir}/#{cmd}"
   end
 end
+
+# This assumes that the major version of libpq that ships with
+# postgres `version` is 5. To be fixed when that is no longer
+# the case.
+%w(
+  libpq.so libpq.so.5
+).each do |lib|
+  link "/opt/local/lib/#{lib}" do
+    to "#{prefix_dir}/lib/#{lib}"
+    not_if "test -e /opt/local/lib/#{lib}"
+  end
+end
