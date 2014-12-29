37478eba6837a1852bc5ac685f1d8db9de4e3af3
Eric Saxby <sax@livinginthepast.org>
2014-12-29 11:07:57 -0800

    Compile all contrib modules
    Make sure that all included contrib modules are available
    for installation, rather than specifying specific ones.

--- recipes/contrib.rb
+++ recipes/contrib.rb
@@ -24,59 +24,8 @@ src_dir = "#{Chef::Config['file_cache_path']}/postgresql-#{version}/contrib"
 prefix_dir  = node['postgres']['prefix_dir'].gsub(/%VERSION%/, version)
 bin_dir     = prefix_dir + '/bin'
 
-# pg_upgrade
-
-execute 'install pg_upgrade_support' do
-  cwd "#{src_dir}/pg_upgrade_support"
+execute 'install contrib extensions' do
+  cwd src_dir
   command 'make && make install'
   not_if { File.exists?("#{bin_dir}/pg_upgrade") }
-end
-
-execute 'install pg_upgrade' do
-  cwd "#{src_dir}/pg_upgrade"
-  command 'make && make install'
-  not_if { File.exists?("#{bin_dir}/pg_upgrade") }
-end
-
-# pgbench
-
-execute 'install pgbench' do
-  cwd "#{src_dir}/pgbench"
-  command 'make && make install'
-  not_if { File.exists?("#{bin_dir}/pgbench") }
-end
-
-# pg_stat_statements
-
-ruby_block 'pg_stat_statements info' do
-  block do
-    Chef::Log.info <<-EOL
-
-    #################################################
-
-    Installing pg_stat_statements contrib extension
-
-    In order to enable this extension, log into your
-    Postgres instance and run the following command:
-
-      CREATE EXTENSION pg_stat_statements;
-
-    #################################################
-    EOL
-  end
-  not_if { File.exists?("#{prefix_dir}/lib/pg_stat_statements.so") }
-end
-
-execute 'install pg_stat_statements' do
-  cwd "#{src_dir}/pg_stat_statements"
-  command 'make && make install'
-  not_if { File.exists?("#{prefix_dir}/lib/pg_stat_statements.so") }
-end
-
-# hstore
-
-execute 'install hstore' do
-  cwd "#{src_dir}/hstore"
-  command 'make && make install'
-  not_if { File.exists?("#{prefix_dir}/lib/hstore.so") }
 end
