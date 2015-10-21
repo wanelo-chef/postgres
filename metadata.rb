ea78108876cb28bf5ad5eb5497164b21de0f3f3e
Eric Saxby <eric@wanelo.com>
2015-10-21 14:45:10 -0700

    2.17.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@wanelo.com'
 license          'Apache 2.0'
 description      'Installs/Configures postgres from sources, optimized for SmartOS, and ready for streaming replication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.17.1'
+version          '2.17.2'
 
 supports 'smartos'
 
